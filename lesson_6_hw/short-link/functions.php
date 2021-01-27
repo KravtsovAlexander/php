bc7977fb6c9d768ee9c1232857fabc1fc102b598
KravtsovAlexander <linejjka@gmail.com>
2021-01-27 21:46:22 +0300

    lesson_6_hw

--- lesson_6_hw/short-link/functions.php
+++ lesson_6_hw/short-link/functions.php
@@ -29,18 +29,14 @@ function get_id(string $db_file, string $url)
 function add_link(string $db_file, string $url, string $id)
 {
     $data = file_get_contents($db_file);
-    if ($data) {
-        $arr = json_decode($data, JSON_OBJECT_AS_ARRAY);
-    } else {
-        $arr = [];
-    }
+    $arr = $data ? json_decode($data, JSON_OBJECT_AS_ARRAY) : [];
     $new_link = [
         'url' => $url,
         'id' => $id,
         'date' => date(DATE_RSS)
     ];
 
-    array_push($arr, $new_link);
+    $arr[] = $new_link;
     file_put_contents($db_file, json_encode($arr));
     return true;
 }
